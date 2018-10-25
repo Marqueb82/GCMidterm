ab283a4bd7d86386e1cef006e80d5569a3d8a78a
Twani Dhar <twani.dhar@gmail.com>
2018-10-25 14:27:06 -0400

    update

--- src/gc_midterm/LibraryApp.java
+++ src/gc_midterm/LibraryApp.java
@@ -1,10 +1,36 @@
 package gc_midterm;
 
+import java.util.Scanner;
+
 public class LibraryApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner userInput = new Scanner (System.in);
+	
+		
+		System.out.println("Welcome to Grand Chirpus library!");
+		System.out.println("What can we do for you today?");
+		
+		boolean valid = false;
+		
+		do {
+			System.out.println(" 1.	See a list of books.");
+			System.out.println(" 2. Select a genre.");
+			System.out.println(" 3. Search by author.");
+			System.out.println(" 4. Search by title keyword.");
+			System.out.println(" 5. Select a book to checkout.");
+			System.out.println(" 6. Return a book.");
+			System.out.println(" 7. Exist Grand Chirpus Directory.");
+			
+			int userResponse = userInput.nextInt();
+			
+			
+			
+		}while (!valid);
+		
+		
+		userInput.close();
 	}
 
 }

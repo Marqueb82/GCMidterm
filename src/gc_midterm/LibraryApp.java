0064bd6226ef9b7e7e65f28cedc4e133ccb76ecc
Mariah Hall <hallmar94@gmail.com>
2018-10-27 09:46:54 -0400

    fixed user check out function, and added return a book method MH

--- src/gc_midterm/LibraryApp.java
+++ src/gc_midterm/LibraryApp.java
@@ -2,6 +2,9 @@ package gc_midterm;
 
 import java.io.IOException;
 import java.text.ParseException;
+import java.time.LocalDate;
+import java.time.Month;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -10,10 +13,11 @@ public class LibraryApp {
 
 	private static BookTextFile btf = new BookTextFile();
 	private static Scanner userInput = new Scanner(System.in);
+	private static final LocalDate PRINCE = LocalDate.of(1999, Month.JANUARY, 1);
 
 	public static void main(String[] args) throws IOException, ParseException {
 
-		List<Book> ourBooks = btf.showBooks();
+		List<Book> completeList = btf.showBooks();
 		System.out.println("Welcome to Grand Chirpus library!");
 		System.out.println("How can we help you today?");
 
@@ -33,6 +37,7 @@ public class LibraryApp {
 
 			System.out.print("Please select the number that you would like explore: ");
 			userResponse = userInput.nextInt();
+			System.out.println();
 			userInput.nextLine();
 
 			switch (userResponse) {// the value being checked for
@@ -40,36 +45,44 @@ public class LibraryApp {
 				// method to list books
 				System.out.println("Here is a list of our books--");
 				System.out.println("==============================");
-				printOutBooks(ourBooks);
+				printOutBooks(completeList);
 				break;
 			case 2:
 				// TODO method for genres: Self-Help, Fiction, Non-Fiction
 				// may need to check how we specify txt genre--may need one genre
+				// TODO was thinking that we could use the split method using ", " to make a set
+				// that gets rid of duplicates of the genres? -MH
 				System.out.println("What would you like to search by: ");
 				String userSearch = userInput.nextLine();
 				System.out.println("Results--");
-				searchBooks(ourBooks, userSearch);
+				searchBooks(completeList, userSearch);
 				break;
 			case 3:
 				// TODO Author
-				System.out.println("Enter author's name: ");
+				System.out.print("Enter author's name: ");
 				String authorName = userInput.nextLine();
 				System.out.println("Results--");
-				searchByAuthor(ourBooks, authorName);
+				searchByAuthor(completeList, authorName);
 				break;
 			case 4:
 				// calls method to find keywords
 				keywordInBooklist();
 				break;
 			case 5:
+				// TODO possibly just make a list of books that are currently onshelf??
 				// calls method to check out book
-				printOutBooks(ourBooks);
-				System.out.println("Which book");
+				printOutBooks(completeList);
+				System.out.print("Which book would you like to check out? ");
 				int choice = userInput.nextInt();
-				checkOutBook(ourBooks, choice);
+				completeList = checkOutBook(completeList, choice);
 				break;
 			case 6:
-				// TODO return a book
+				// TODO possibly just make a list of books that are currently check out??
+				// allows user to return a book
+				printOutBooks(completeList);
+				System.out.print("Which book would you like to return? ");
+				choice = userInput.nextInt();
+				completeList = returnBook(completeList, choice);
 				break;
 			case 7:
 				System.out.println("Enjoy reading!");
@@ -132,7 +145,7 @@ public class LibraryApp {
 					}
 
 					if (keywordIncluded == null) {
-						System.out.println("Sorry, there are no authors containing that keyword.");
+						System.out.println("Sorry, we don't have any inventory containing that keyword.");
 					}
 
 				}
@@ -147,12 +160,13 @@ public class LibraryApp {
 		} while (!isValid);
 	}
 
-	public static void searchBooks(List<Book> ourBooks, String search) {
+	public static void searchBooks(List<Book> completeList, String search) {
 		List<Book> searchedBookList = new ArrayList<>();
 
-		for (Book b : ourBooks) {
-			if (b.getGenre().equals(search) || b.getAuthor().equals(search) || b.getTitle().equals(search)) {
-				searchedBookList.add(b);
+		for (Book sortBook : completeList) {
+			if (sortBook.getGenre().equals(search) || sortBook.getAuthor().equals(search)
+					|| sortBook.getTitle().equals(search)) {
+				searchedBookList.add(sortBook);
 			}
 		}
 
@@ -163,12 +177,12 @@ public class LibraryApp {
 
 	}
 
-	public static void searchByAuthor(List<Book> ourBooks, String search) {
+	public static void searchByAuthor(List<Book> completeList, String search) {
 		List<Book> searchedBookList = new ArrayList<>();
 
-		for (Book b : ourBooks) {
-			if (b.getAuthor().equals(search)) {
-				searchedBookList.add(b);
+		for (Book sortBook : completeList) {
+			if (sortBook.getAuthor().equals(search)) {
+				searchedBookList.add(sortBook);
 			}
 		}
 
@@ -179,11 +193,41 @@ public class LibraryApp {
 
 	}
 
-	public static void checkOutBook(List<Book> ourBooks, int choice) throws IOException, ParseException {
-		ourBooks.get(choice - 1).setAvailability(BookStatus.CHECKEDOUT);
-		System.out.println(ourBooks.get(choice - 1).toString());
-		// method incomplete only returns changed object in text file
-		btf.rewriteFile(ourBooks);
+	public static List<Book> checkOutBook(List<Book> completeList, int choice) throws IOException, ParseException {
+		// sorts through list of all books to find the book requested by user and sets a
+		// due date if book is on shelf, if book is already checked out, it lets the
+		// user know
+		Book ourBook = completeList.get(choice - 1);
+
+		if (ourBook.getAvailability().equals(BookStatus.ONSHELF)) {
+			ourBook.setAvailability(BookStatus.CHECKEDOUT);
+			LocalDate today = LocalDate.now();
+			LocalDate dueDate = today.plus(14, ChronoUnit.DAYS);
+			ourBook.setDueDate(dueDate);
+		} else if (completeList.get(choice - 1).getAvailability().equals(BookStatus.CHECKEDOUT)) {
+			System.out.println("Sorry, that book has already been checked out. It is due back to the library "
+					+ ourBook.getDueDate() + ".");
+		}
+		System.out.println(completeList.get(choice - 1).toString());
+		System.out.println(completeList);
+		return completeList;
+		// TODO method incomplete only returns changed object in text file
+//		btf.rewriteFile(ourBooks);
+	}
+
+	public static List<Book> returnBook(List<Book> completeList, int choice) throws IOException, ParseException {
+		// checks book at index and sets to onshelf if it was checked out
+		Book ourBook = completeList.get(choice - 1);
+
+		if (ourBook.getAvailability().equals(BookStatus.CHECKEDOUT)) {
+			ourBook.setAvailability(BookStatus.ONSHELF);
+			ourBook.setDueDate(PRINCE);
+		} else if (completeList.get(choice - 1).getAvailability().equals(BookStatus.ONSHELF)) {
+			System.out.println("That book is already available!");
+		}
+		System.out.println(completeList.get(choice - 1).toString());
+		System.out.println(completeList);
+		return completeList;
 	}
 
 //	public List<Book> searchGenre(List<Book> ourBooks) {
